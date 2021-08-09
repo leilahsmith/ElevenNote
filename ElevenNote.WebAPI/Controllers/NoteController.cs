ef2319bffed528d213accbb4665a47a0dc4225fd
leilahsmith <leilah.sdev@gmail.com>
2021-08-08 17:37:56 -0700

    Added controller delete method and delete method in NoteService.

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -58,6 +58,16 @@ namespace ElevenNote.WebAPI.Controllers
 
             return Ok();
         }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateNoteService();
+
+            if (!service.DeleteNote(id))
+                return InternalServerError();
+
+            return Ok();
+        }
     }
 
   
