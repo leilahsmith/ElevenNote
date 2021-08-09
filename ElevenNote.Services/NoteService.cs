ffd544ae92316ebcf326f1880571eecef3cc56ca
leilahsmith <leilah.sdev@gmail.com>
2021-08-08 17:18:36 -0700

    Added Get Controller method and GetNoteById service method.

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -37,6 +37,7 @@ namespace ElevenNote.Services
 
         public IEnumerable<NoteListItem> GetNotes()
         {
+
             using (var ctx = new ApplicationDbContext())
             {
                 var query =
@@ -57,5 +58,26 @@ namespace ElevenNote.Services
 
             }
         }
+        public NoteDetail GetNoteById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Notes
+                        .Single(e => e.NoteId == id && e.OwnerId == _userId);
+                return
+                    new NoteDetail
+                    {
+                        NoteId = entity.NoteId,
+                        Title = entity.Title,
+                        Content = entity.Content,
+                        CreatedUtc = entity.CreatedUtc,
+                        ModifiedUtc = entity.ModifiedUtc
+                    };
+            }
+        }
+
+
     }
 }
